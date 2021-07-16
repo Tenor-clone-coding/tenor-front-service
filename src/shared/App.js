25926bd7181e85bd8dec3b6ec78ce9ad1e286f56
asg0416 <73174694+asg0416@users.noreply.github.com>
2021-07-16 19:10:15 +0900

    [#1] feat: front 기본 공통 뼈대 코드

---
+++ src/shared/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+import { Home } from "../pages";
+
+import { Route } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
+
+
+
+function App() {
+
+  return (
+    <React.Fragment>
+        <ConnectedRouter history={history}>
+          <Route path="/" exact component={Home} />
+        </ConnectedRouter>
+    </React.Fragment>
+  );
+}
+
+export default App;
