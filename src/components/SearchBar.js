291e098e272fea20fd7ea46aa6fab4c605de3c66
jk <166354@gmail.com>
2021-07-17 18:15:59 +0900

    [#7] feat: 헤더 반응형, 로고, 메뉴 기능 추가

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,33 @@
 import styled from "styled-components";
 import SearchIcon from "@material-ui/icons/Search";
+import logo_white from "../logo_white.svg";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [isScrollDown, setIsScrollDown] = useState(false);
+
+  const handleScrollDown = (e) => {
+    if (e.target.documentElement.scrollTop !== 0) {
+      setIsScrollDown(true);
+    } else {
+      setIsScrollDown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollDown);
+    return () => window.removeEventListener("scroll", handleScrollDown);
+  }, []);
+
   return (
     <Container>
-      <Span>tenor</Span>
-      <Form>
-        <Input></Input>
+      <Span isScrollDown={isScrollDown}>
+        <img src={logo_white} alt="logo_basic" />
+      </Span>
+
+      <Form isScrollDown={isScrollDown}>
+        <Input placeholder="Search for GIFs and Stickers"></Input>
         <Button>
           <SearchIcon fontSize="large" />
         </Button>
@@ -16,8 +37,9 @@ const SearchBar = () => {
 };
 
 const Container = styled.div`
+  height: 75px;
   width: 100vw;
-  background-color: #2e93e6;
+  background-color: #4cafff;
   display: flex;
   box-sizing: border-box;
   padding: 10px 5%;
@@ -34,10 +56,13 @@ const Input = styled.input`
   padding: 1em 1.5em;
   border: none;
   border-radius: 3px;
+  font-size: 15px;
+  font-weight: 600;
 `;
 
 const Button = styled.button`
   position: absolute;
+  margin-right: 15px;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
@@ -51,12 +76,26 @@ const Form = styled.form`
   display: flex;
   width: 100%;
   position: relative;
+  transition: width 0.5s ease-in-out;
+  ${({ isScrollDown }) => (isScrollDown ? "width: 90%;" : "width: 100%;")}
 `;
 
 const Span = styled.span`
-  font-size: 3rem;
-  color: white;
   margin-right: 1em;
+  transition: opacity 0.5s ease-in-out;
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  transform: translateY(-50%);
+  ${({ isScrollDown }) => (isScrollDown ? `opacity: 1; ` : "opacity: 0; ")};
+  margin-right: 1em;
+  & img {
+    width: 80px;
+    aspect-ratio: auto 80 / 22;
+    height: 22px;
+    top: 50%;
+    transform: translateY(0%);
+  }
 `;
 
 export default SearchBar;
